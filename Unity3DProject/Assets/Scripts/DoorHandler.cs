335211e5e163ab74c0010f3824f45488bfa94d6d
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2021-06-23 23:51:45 +0300

    Skybox added, DoorTrigger created

---
+++ Unity3DProject/Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DoorHandler : MonoBehaviour
+    {
+        private Animator animator;
+
+        void Start()
+        {
+            animator = this.gameObject.GetComponent<Animator>();
+        }
+        
+        void Update()
+        {
+        }
+
+        public void Trigger()
+        {
+            if (animator != null)
+            {
+                animator.SetTrigger("Toggle");
+            }
+        }
+    }
+}
---
+++ Unity3DProject/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DoorManager : MonoBehaviour
+    {
+        public GameObject EntranceDoor;
+
+        private DoorHandler entranceDoorHadler;
+        
+        void Start()
+        {
+            entranceDoorHadler = EntranceDoor.GetComponent<DoorHandler>();
+        }
+        
+        void Update()
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                entranceDoorHadler.Trigger();
+            }
+        }
+    }
+}

9465c6033b62e01da7e711d5b295a753b7111cfd
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2021-06-25 04:07:50 +0300

    Interaction added for doors

--- Unity3DProject/Assets/Scripts/DoorManager.cs
+++ Unity3DProject/Assets/Scripts/DoorManager.cs
@@ -1,23 +1,76 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class DoorManager : MonoBehaviour
     {
-        public GameObject EntranceDoor;
-
         private DoorHandler entranceDoorHadler;
+        private DoorHandler[] components;
+        private GameObject player;
+        private readonly float maxDistanceForIteract = 5;
         
         void Start()
         {
-            entranceDoorHadler = EntranceDoor.GetComponent<DoorHandler>();
+            components = GameObject.FindObjectsOfType<DoorHandler>();
+            player = GameObject.FindWithTag("Player");
+
+            /*
+            foreach(IsObject comp in components)
+                isObject.Add(comp.gameObject);
+                */
+            
+            // entranceDoorHadler = EntranceDoor.GetComponent<DoorHandler>();
         }
         
         void Update()
         {
-            if (Input.GetKeyDown("e"))
+            if (Input.GetKeyDown("e") && components != null && components.Length>0)
             {
-                entranceDoorHadler.Trigger();
+                var handler = GetDoorHandler();
+                if (handler == null)
+                    return;
+                
+                handler.Trigger();
+            }
+        }
+
+        [CanBeNull]
+        private DoorHandler GetDoorHandler()
+        {
+            Vector3? playerPosition = GetPlayerPosition();
+            if (playerPosition == null)
+                return null;
+
+            float shortestDistance = -1;
+            DoorHandler nearestDoorHandler = null;
+            
+            foreach (var door in components)
+            {
+                float distance = Vector3.Distance(door.transform.position, playerPosition.Value);
+
+                if (shortestDistance < 0 || distance < shortestDistance)
+                {
+                    shortestDistance = distance;
+                    nearestDoorHandler = door;
+                }
+            }
+
+            if (shortestDistance <= maxDistanceForIteract && shortestDistance >= 0 && nearestDoorHandler != null)
+                return nearestDoorHandler;
+            else
+                return null;
+        }
+
+        private Vector3? GetPlayerPosition()
+        {
+            if (player != null)
+            {
+                return player.transform.position;
+            }
+            else
+            {
+                return null;
             }
         }
     }
